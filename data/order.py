c41526a63d53a2a38a354fe0aafc17d27e3287c7
wangqianwen0418 <wangqianwen0418@163.com>
2016-11-12 11:04:18 +0800

    version——1

--- data/order.py
+++ data/order.py
@@ -1,5 +1,21 @@
 import json
+with open("./data/newdata.json" ) as json_data:
+    data = json.load(json_data)
+nodes=data['nodes']
+nodeRef={}
 
-with open('data1.json') as data_file:    
-    data = json.load(data_file)
-print(data)
+for uid in nodes:
+    value=nodes[uid]
+    print(uid)
+    item={}
+    item["x"]=value['x']
+    item["y"]=value['y']
+    item["z"]=value['z']
+    item["degree"]=value['degree']
+    item["depth"]=value['depth']
+    item["timestamp"]=value['timestamp']
+    nodeRef[uid]=item;
+
+
+with open('./data/nodeRef.json', 'w') as outfile:
+    json.dump(nodeRef, outfile)
